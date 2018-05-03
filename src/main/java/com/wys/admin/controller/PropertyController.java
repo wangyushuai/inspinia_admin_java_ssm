123bf45ac2a54254fd928caf28f503bfb8556d8a
wangyushuai <wangyushuai@fang.com>
2018-05-03 15:43:31 +0800

    Category Manage All

--- src/main/java/com/wys/admin/controller/PropertyController.java
+++ src/main/java/com/wys/admin/controller/PropertyController.java
@@ -1,7 +1,11 @@
 package com.wys.admin.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * Created by wangyushuai@fang.com on 2018/5/3.
  */
+
+@Controller
 public class PropertyController {
 }
